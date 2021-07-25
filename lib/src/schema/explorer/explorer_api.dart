80282a6ad2fbc7806075995b39ff4acace2914fa
parodyBit <58690522+parodyBit@users.noreply.github.com>
2021-07-25 15:20:47 -0600

    initial explorer api

---
+++ lib/src/schema/explorer/explorer_api.dart
@@ -0,0 +1,63 @@
+
+import 'dart:convert' as convert;
+import 'dart:math';
+
+import 'package:witnet/src/schema/explorer/explorer_status.dart';
+
+import '../node_rpc/get_utxo_info.dart';
+import 'package:http/http.dart' as http;
+
+class ExplorerAPI {
+
+  ExplorerAPI({
+    this.url,
+    this.methods,
+  });
+
+  final String url;
+  final List<String> methods;
+
+  String _buildUrl(String method, Map params){
+    return 'https://$url';
+  }
+
+  String api(String method){
+    return 'api/$method';
+  }
+
+  Future<ExplorerStatus> status() async {
+    Uri urlEndpoint = Uri.https(url, api('status'));
+    int balance = 0;
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(urlEndpoint);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+      convert.jsonDecode(response.body) as Map<String, dynamic>;
+      return ExplorerStatus.fromJson(jsonResponse);
+    }
+  }
+
+  Future<List<Utxo>> getUtxoInfo({String address}) async {
+    Uri urlEndpoint = Uri.https(url, api('utxos'), {'address': address});
+    print('Connecting to $url');
+    int balance = 0;
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(urlEndpoint);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+      convert.jsonDecode(response.body) as Map<String, dynamic>;
+      List<dynamic> utxoList = jsonResponse['utxos'];
+      for (int i = 0; i < utxoList.length; i++){
+        Map<String, dynamic> _utxoMap = utxoList[i];
+        print(_utxoMap);
+        Utxo _utxo = Utxo.fromJson(_utxoMap);
+        print(_utxo.value);
+        utxoList.add(_utxo);
+
+        balance += _utxo.value;
+      }
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+}
---
+++ lib/src/schema/explorer/explorer_status.dart
@@ -0,0 +1,40 @@
+
+import 'dart:convert';
+
+import 'package:witnet/node_rpc.dart';
+
+class ExplorerStatus {
+  ExplorerStatus({
+    this.databaseLastConfirmed,
+    this.databaseLastUnconfirmed,
+    this.databaseMessage,
+    this.syncStatus,
+    this.nodePoolMessage,
+  });
+
+  final List<dynamic> databaseLastConfirmed;
+  final List<dynamic> databaseLastUnconfirmed;
+  final String databaseMessage;
+  final SyncStatus syncStatus;
+  final String nodePoolMessage;
+
+  factory ExplorerStatus.fromRawJson(String str) => ExplorerStatus.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory ExplorerStatus.fromJson(Map<String, dynamic> json) => ExplorerStatus(
+    databaseLastConfirmed: List<dynamic>.from(json["database_last_confirmed"].map((x) => x)),
+    databaseLastUnconfirmed: List<dynamic>.from(json["database_last_unconfirmed"].map((x) => x)),
+    databaseMessage: json["database_message"],
+    syncStatus: SyncStatus.fromJson(json["node_pool"]),
+    nodePoolMessage: json["node_pool_message"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "database_last_confirmed": List<dynamic>.from(databaseLastConfirmed.map((x) => x)),
+    "database_last_unconfirmed": List<dynamic>.from(databaseLastUnconfirmed.map((x) => x)),
+    "database_message": databaseMessage,
+    "node_pool": syncStatus.toJson(),
+    "node_pool_message": nodePoolMessage,
+  };
+}
