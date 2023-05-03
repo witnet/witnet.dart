74f225dcb8baf15d3a267a322a76509bc60be8fb
tommy <tomastrg22@gmail.com>
2023-05-03 18:18:28 +0200

    chore: update block explorer example

--- example/explorer_example.dart
+++ example/explorer_example.dart
@@ -7,7 +7,9 @@ ExplorerClient explorer =
 void main() async {
   try {
     Status status = await explorer.status();
+    PrioritiesEstimate priority = await explorer.valueTransferPriority();
     print("Status: ${status.databaseMessage}");
+    print("Priority ${priority.vttHigh.priority}");
   } on ExplorerException catch (e) {
     print(e.message);
   }
