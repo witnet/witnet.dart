59caf8ffdca1c5879dea0b8bed3f0295d53b8481
gabaldon <maria@otherplane.com>
2024-02-15 09:18:46 +0100

    fix: upgrade cocoapods

--- example/explorer_example.dart
+++ example/explorer_example.dart
@@ -7,7 +7,7 @@ void main() async {
   try {
     dynamic status = await explorer.address(
         tab: "value_transfers",
-        value: "wit1zl7ty0lwr7atp5fu34azkgewhtfx2fl4wv69cw",
+        value: "wit174la8pevl74hczcpfepgmt036zkmjen4hu8zzs",
         page: 1,
         pageSize: 1);
     print("Status: ${status}");
